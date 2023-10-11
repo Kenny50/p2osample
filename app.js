09a30911b9b664f9001a86188942c080ce8914da
chang <kennethchangla@gmail.com>
2023-10-12 01:32:06 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs")
+const YAML = require('yaml')
+
+const file  = fs.readFileSync(`${__dirname}/swagger.yml`, 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.listen(3000, () => {
+    console.log(`Example app listening on port ${3000}`)
+})
